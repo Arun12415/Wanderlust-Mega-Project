9328764905ca7b1327343593cb435d364c9ba3f7
Arun Rao <iarunrao1.in@gmail.com>
2025-10-20 13:26:46 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "allow_user_to_connect" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["13.127.239.99"]
   }
 
   ingress {
